07b554900f3f4af103027ae79951f24617b6dcde
kevinsosborne <kevinsosborne@gmail.com>
2017-10-03 11:31:53 -0700

    CirQueue: Front

---
+++ Chapter6 - Queues and Stacks/CirQueue: Front.js
@@ -0,0 +1,19 @@
+// Return (not remove) the queue's front value.
+
+function CirQueue(cap){
+    this.head = 0;
+    this.tail = 0;
+    this.capacity = cap;
+    this.arr = [];
+    this.front = function(){
+        if(!this.arr[this.head]){
+            return "Empty Array";
+        }
+        else{
+        arr[head];
+        }
+    }
+}
+
+var q = new CirQueue(10);
+console.log(q.front());
