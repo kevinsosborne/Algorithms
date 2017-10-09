f0c6a341fc50e48c7be6749d4d515e8d61cdc7c9
Kevin Osborne <kevinsosborne@gmail.com>
2017-10-09 15:26:01 -0700

    Balance Index

---
+++ Chapter 7 - Arrays, Part II/Balance Index.js
@@ -0,0 +1,20 @@
+// Here, a balance point is on an index, not between indicies. Return the balance index, where sums are equal on either side (exclude its own value), Return -1 if none exist. Ex.: [-2,5,7,0,3] -> 2, but [9, 9] -> -1
+
+function balanceIndex(arr){
+    var totalCount = arr[0];
+    for (var i = 1; i < arr.length; i++){
+        totalCount += arr[i];
+    }
+    var leftCount = 0;
+    for(var idx = 0; idx < arr.length; idx++){
+        totalCount -= arr[idx];
+        if(leftCount == totalCount){
+            return idx;
+        }
+        leftCount += arr[idx];        
+        
+    }
+    return -1;
+}
+
+console.log(balanceIndex([-2,5,7,0,3]));
