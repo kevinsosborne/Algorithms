6346b604f4cab494739062742fa9e6264bc72141
Kevin Osborne <kevinsosborne@gmail.com>
2017-10-14 08:45:04 -0700

    Min of Sorted-Rotated

---
+++ Chapter 7 - Arrays, Part II/Min of Sorted-Rotated.js
@@ -0,0 +1,2 @@
+// You will be given a numerical array that has first been sorted, then rotated by an unknown amount. Find and return the minimum value in that array. Don't use built-in functions. Given the input array ["Gigli","Jay is cool","Mavis","Phoebe","Thurber","Anna","Celeste","Elon"], return "Anna". 
+
