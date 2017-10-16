1b19410777d8b2d1e40c8cc2c98a11f8bffed329
Kevin Osborne <kevinsosborne@gmail.com>
2017-10-16 10:33:25 -0700

    Min of Sorted Array Rotated

--- Chapter 7 - Arrays, Part II/Min of Sorted-Rotated.js
+++ Chapter 7 - Arrays, Part II/Min of Sorted-Rotated.js
@@ -1,47 +1,34 @@
 // You will be given a numerical array that has first been sorted, then rotated by an unknown amount. Find and return the minimum value in that array. Don't use built-in functions. Given the input array ["Gigli","Jay is cool","Mavis","Phoebe","Thurber","Anna","Celeste","Elon"], return "Anna". 
 
-function binarySearchMin(array){
-   var alphabet = {
-       a: 1,
-       b: 2,
-       c: 3,
-       d: 4,
-       e: 5,
-       f: 6,
-       g: 7,
-       h: 8,
-       9: i,
-       10: j,
-       11: k,
-       12: l,
-       13: m,
-       14: n,
-       15: o,
-       16: p,
-       17: q,
-       18: r,
-       19: s,
-       20: t,
-       21: u,
-       22: v,
-       23: w,
-       24: x,
-       25: y,
-       26: z
-   }
-   var first = 0;
-   var last = array.length -1;
-   var mid = (first + last)/2;
-   while(first <= last){  
-    if(array[mid].charCodeAt(0) == "A"){
-            return array[mid];
-        }
-    if(array[mid].charCodeAt(0) != "A"){
+function minOfSortedRotated(arr) {
+    var left = 0
+    var mid = Math.floor((arr.length)/2)
+    console.log(arr[mid]);
+    console.log((arr.length)/2);
+    var right = arr.length - 1
 
+    while (mid != left) {
+        
+        if (arr[mid] > arr[right]) {
+            console.log(arr[mid] + " " + arr[right]);
+            left = mid
         }
-   }
+        else if (arr[left] > arr[mid]) {
+            right = mid
+        }
+        mid = Math.floor( (right - left)/2) + left
+    }
 
+    if (arr[mid] <= arr[right]) {
+        return arr[mid]
+    }
+    else {
+        return arr[right]
+    }
 }
 
-var arr = ["Gigli","Jay is cool","Mavis","Phoebe","Thurber","Anna","Celeste","Elon"];
-console.log(arr.sort());
+console.log( minOfSortedRotated(['Gigli','Jay is cool','Mavis','Phoebe','Thurber','Anna','Celeste',"Elon"]) )
+
+var g = 'Gigli';
+var j = 'Jay is cool';
+console.log(g < j);
