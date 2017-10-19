dea5840cc59ddf8a64c0a671b7943e6e1f0832a9
Kevin Osborne <kevinsosborne@gmail.com>
2017-10-19 15:28:24 -0700

    Array: Buffer Copy

--- Chapter 7 - Arrays, Part II/Array: Buffer Copy.js
+++ Chapter 7 - Arrays, Part II/Array: Buffer Copy.js
@@ -1,12 +1,49 @@
 // Create arrBufferCopy(sourceArr, destArr, sourceStartIdx, destStartIdx, numVals) to copy numVals values starting at sourceArr[sourceStartIdx] to destArr[destStartIdx] etc. DO not lengthen destArr, nor read off the end of sourceArr.
 
 function arrBufferCopy(sourceArr, destArr, sourceStartIdx, destStartIdx, numVals){
-    for(sourceStartIdx; sourceStartIdx < sourceArr.length; sourceStartIdx++){
-        numVals = sourceArr[sourceStartIdx];
-        destArr[destStartIdx] = numVals;
-        destStartIdx++;
+    var i = sourceStartIdx;
+    var count = 1;
+    while(count <= numVals){   
+            
+
+        if(i == sourceArr.length){
+            i = 0;
+            
+            if(destStartIdx == destArr.length){
+                destStartIdx = 0;
+                // console.log("source Arr" + sourceArr[i] + " i " + i);
+                
+                destArr[destStartIdx] = sourceArr[i];            
+            }
+            else{
+                // console.log("source Arr" + sourceArr[i] + " i " + i);
+                
+                destArr[destStartIdx] = sourceArr[i];            
+                destStartIdx++;
+            }
+        }   
+        
+
+        
+       
+        else{
+            console.log("source Arr " + sourceArr[i] + " i " + i);
+            
+            if(destStartIdx == destArr.length){
+                destStartIdx = 0;
+                destArr[destStartIdx] = sourceArr[i];            
+            }
+            else{
+                destArr[destStartIdx] = sourceArr[i];            
+                destStartIdx++;
+            }
+            i++;
+        }
+        count++;
     }
+
+
     return destArr;
 }
 
-console.log(arrBufferCopy([1,2,3,4,5],[],0,5,0));
+console.log(arrBufferCopy([9,8,7,6,5,4,3,2,1],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"],8,12,10));
---
+++ Chapter 7 - Arrays, Part II/Faster Factorial.js
@@ -0,0 +1,1 @@
+// Remember Factorial(num)? It returned the product of positive integers from 1 to num. Examples: Factorial(1) = 1, Factorial(2) = 2. Facotiral(3) = 6. For these purposes, Factorial(0) = 1. Recreate this function, but usa  time-space tradeoff to accelerate the average running time.
