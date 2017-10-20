c7d32d824eb3f7dcb6281f2dccf4e8de9af13028
Kevin Osborne <kevinsosborne@gmail.com>
2017-10-19 17:32:44 -0700

    Faster Factoiral

--- Chapter 7 - Arrays, Part II/Faster Factorial.js
+++ Chapter 7 - Arrays, Part II/Faster Factorial.js
@@ -1,1 +1,40 @@
 // Remember Factorial(num)? It returned the product of positive integers from 1 to num. Examples: Factorial(1) = 1, Factorial(2) = 2. Facotiral(3) = 6. For these purposes, Factorial(0) = 1. Recreate this function, but usa  time-space tradeoff to accelerate the average running time.
+
+
+
+class fasterFactorial{
+    constructor(){
+        this.integers = {};
+    }
+    Factorial(num){
+        if(!this.integers[num]){
+            var val = 1;
+            var i = 2;
+            while(i <= num){
+                val *= i
+                i++;
+            }
+            this.integers[num] = val;
+        }
+        else{
+            this.integers[num];
+        }
+    return this.integers;
+    }
+}
+
+var f = new fasterFactorial();
+console.log(f.Factorial(1));
+console.log(f.Factorial(2));
+console.log(f.Factorial(3));
+console.log(f.Factorial(2));
+console.log(f.Factorial(4));
+console.log(f.Factorial(500));
+
+var start = new Date();
+
+setTimeout(function (argument) {
+    console.log(f.Factorial(500));
+    var end = new Date() - start;
+    console.info("Execution time: %dms", end);
+}, 1000);
